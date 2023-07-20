9d50efc79a2fd8b128e8d867a57bd98bee742e11
Priyal lohar <priyallohar27@gmail.com>
2023-07-20 17:22:14 +0530

    update :- added svg for mobile design

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const menuBtn = document.querySelector("#btn")
 const mobileNav = document.querySelector(".mobile_nav")
+const WorkingSvg = document.querySelector(".working_svg ")
 menuBtn.addEventListener("click",function(){
     if (mobileNav.style.display === "none") {
         mobileNav.style.display = "flex"; 
+        WorkingSvg.style.display = "none"
       } else {
         mobileNav.style.display = "none";
+        WorkingSvg.style.display = "Block"
       }
 })
