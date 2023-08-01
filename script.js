39b9551784a370e96ef4416ba7e295ed97bbe067
Priyal lohar <priyallohar27@gmail.com>
2023-08-01 16:11:54 +0530

    changes in nav of mobile screen

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const menuBtn = document.querySelector("#btn")
 const mobileNav = document.querySelector(".mobile_nav")
 const WorkingSvg = document.querySelector(".working_svg ")
+const WorkingSvgdiv = document.querySelector(".working_svg_div")
 menuBtn.addEventListener("click",function(){
     if (mobileNav.style.display === "none") {
         mobileNav.style.display = "flex"; 
         WorkingSvg.style.display = "none"
+        WorkingSvgdiv.style.display = "none"
       } else {
         mobileNav.style.display = "none";
         WorkingSvg.style.display = "Block"
+        WorkingSvgdiv.style.display = "flex"
       }
 })
